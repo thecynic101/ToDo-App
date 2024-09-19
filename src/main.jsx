4dddac1bd6864d8db1222aee46ea40831c0b1819
thecynic101 <oscaralex.maestro@gmail.com>
2024-09-19 13:52:48 +0100

    first-commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//eslint-disable-next-line
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App tasks={DATA} />
+  </StrictMode>,
+)
